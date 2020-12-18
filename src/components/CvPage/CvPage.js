288dd5af824dc80a600d4cf064345d6a229b299e
Mirra-k <37514488+Mirra-k@users.noreply.github.com>
2020-12-18 19:01:59 +0300

    Update CvPage.js

--- src/components/CvPage/CvPage.js
+++ src/components/CvPage/CvPage.js
@@ -3,25 +3,12 @@ import './CvPage.css';
 
 export class CvPage extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: [],
-            noNewInvites: true,
-            invitesCheckedCnt: 0
-        };
-    }
 
     render() {
         return (
             <React.Fragment>
-                {this.state.noNewInvites ? <h1 className={"page-msg"}>NO INFORMATION</h1> :
-                    <div className={"cv"}>
-                        <div className={"container"}>
-                            {this.state.data.map(
-                            )}
-                        </div>
-                    </div>
+                {
+                    <h1 className={"page-msg"}>NO INFORMATION</h1>
                 }
             </React.Fragment>
         );
